6846defcff95d6e07b4adf6f5512855b0257fc8b
chimum2006 <phamchimum00@gmail.com>
2025-07-13 13:22:43 +0700

    Fix PORT cho Flask chạy trên Render

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-import cloudscraper
+import cloudscraper, os
 from DATA import get_info
 scraper = cloudscraper.create_scraper()
 app = Flask(__name__)
@@ -9,6 +9,9 @@ app.register_blueprint(get_info)
 def hello():
     return jsonify({"message": "Xin Chào Bạn Đến Với Api !"}), 200
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
